4c59ddbe6bb701feb19af595e58af7ad01d7a7a6
LithumHelitha <155241749+LithumHelitha@users.noreply.github.com>
2024-11-03 22:13:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "YcgQ0KqI#XVKcXV4Do6zVmIwjA9vlMnXfq6HK5EVsWnoGMIpfnH4",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
